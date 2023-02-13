4b274d348e6c25bc3ceed9983a1793f5eff98d3f
Derex Wangmang <66037338+derexwangmang@users.noreply.github.com>
2023-02-13 15:52:33 -0600

    Added carousel with tops, integrated through Firebase

---
+++ src/pages/Carousel.jsx
@@ -0,0 +1,22 @@
+import Carousel from "react-bootstrap/Carousel";
+import "../styles/carousel.css";
+
+const MyCarousel = ({ data }) => {
+  return (
+    <div className="carousel">
+      <Carousel>
+        {data.map((imageLink) => (
+          <Carousel.Item>
+            <img
+              className="w-100 shadow-1-strong rounded mb-4"
+              src={imageLink}
+              alt="Slide"
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default MyCarousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import MyCarousel from "./Carousel";
+import { useDbData } from "../utils/firebase";
 
 const Home = () => {
-  return <div className="home-container">OutfitOfTheDay</div>;
+  const [tops] = useDbData("/tops");
+  const [bottoms] = useDbData("/bottoms");
+
+  if (!tops) {
+    return <div></div>;
+  }
+  return (
+    <div>
+      {/* OutfitOfTheDay */}
+      <MyCarousel data={tops}></MyCarousel>
+    </div>
+  );
 };
 
 export default Home;
