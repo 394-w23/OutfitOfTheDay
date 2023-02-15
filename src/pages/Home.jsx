b0c11fddbccbb422fb34b5d33fdbd45a9fa87ef0
Celinadhh <celinaszhao@gmail.com>
2023-02-14 19:44:34 -0600

    dress button works

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,8 @@ const Home = () => {
   const [tops] = useDbData("/tops");
   const [bottoms] = useDbData("/bottoms");
   const [shoes] = useDbData("/shoes");
+  const [dresses] = useDbData("/dress");
+  const [jackets] = useDbData("/jacket");
 
   const [dress, setDress] = useState(false);
   const [jacket, setJacket] = useState(false);
@@ -34,14 +36,16 @@ const Home = () => {
         type="switch"
         onClick={() => handleDress()}
     />
-    <Form.Check
+    {/* <Form.Check
         inline="true"
         label="Jacket?"
         type="switch"
         onClick={() => handleJacket()}
-    />
+    /> */}
     {tops && !dress && <MyCarousel data={tops}></MyCarousel>}
+    {/* {jackets && jacket && <MyCarousel data={jackets}></MyCarousel>} */}
     {bottoms && !dress && <MyCarousel data={bottoms}></MyCarousel>}
+    {dresses && dress && <MyCarousel data={dresses}></MyCarousel>}
     {shoes && <MyCarousel data={shoes}></MyCarousel>}</div>;
 };
 
