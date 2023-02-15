6a039bcba82b7f259eace5021763514bc76de804
Camilo <camilochafloque@gmail.com>
2023-02-14 19:21:11 -0600

    Added css styles to closet screen

--- src/pages/Closet.jsx
+++ src/pages/Closet.jsx
@@ -1,31 +1,36 @@
 import React, { useState } from "react";
 
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import { useDbData } from "../utils/firebase";
 
 import MyCard from "../components/Card";
-
-
-
+import { Container } from "react-bootstrap";
 
 const Closet = () => {
   const [tops] = useDbData("/tops");
+  const [jackets] = useDbData("/tops");
   const [bottoms] = useDbData("/bottoms");
   const [shoes] = useDbData("/shoes");
+  const [dresses] = useDbData("/tops");
 
-
-  return <div>
-    <h3>Tops</h3>
-    {tops && <MyCard data={tops}> </MyCard>}
-    <h3>Bottoms</h3>
-    {bottoms && <MyCard data={bottoms}> </MyCard>}
-    <h3>Shoes</h3>
-    {shoes && <MyCard data={shoes}> </MyCard>}
-
-
-  </div>;
+  return (
+    <Container className="p-3 mb-10">
+      <h3>My Closet</h3>
+      <hr />
+      <h5 className="text-muted mt-3">Tops</h5>
+      {tops && <MyCard data={tops}> </MyCard>}
+      <h5 className="text-muted mt-3">Jackets</h5>
+      {jackets && <MyCard data={jackets}> </MyCard>}
+      <h5 className="text-muted mt-3">Bottoms</h5>
+      {bottoms && <MyCard data={bottoms}> </MyCard>}
+      <h5 className="text-muted mt-3">Dresses</h5>
+      {dresses && <MyCard data={dresses}> </MyCard>}
+      <h5 className="text-muted mt-3">Shoes</h5>
+      {shoes && <MyCard data={shoes}> </MyCard>}
+    </Container>
+  );
 };
 
 export default Closet;
