14556ab7a5e33d6ca2fd7d0035863b041ef0d249
Camilo <camilochafloque@gmail.com>
2023-02-21 14:42:00 -0600

    Refactored home screen to display new closet structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ import { useDbData, useDbUpdate } from "../utils/firebase";
 import { useProfile } from "../utils/userProfile";
 import { v4 as uuidv4 } from "uuid";
 import getMockUser from "../utils/mockUser";
-import axios from 'axios';
+import axios from "axios";
 
 const Home = () => {
   const [weather, setWeather] = useState([]);
@@ -26,23 +26,19 @@ const Home = () => {
         setWind(data["current_weather"]["windspeed"]);
       })
       .catch((err) => console.error(err));
-  }, []);  
-  
-  const user = getMockUser();
+  }, []);
 
-  const [tops] = useDbData("/tops");
-  const [bottoms] = useDbData("/bottoms");
-  const [shoes] = useDbData("/shoes");
-  const [dresses] = useDbData("/dress");
-  const [jackets] = useDbData("/jacket");
+  const user = getMockUser();
+  const [closet] = useDbData("/closet");
+
   const [updateData] = useDbUpdate("/");
 
   const [dress, setDress] = useState(false);
   const [jacket, setJacket] = useState(false);
 
-  const [selectedTop, setSelectedTop] = useState(1);
-  const [selectedBottoms, setSelectedBottoms] = useState(1);
-  const [selectedShoes, setSelectedShoes] = useState(1);
+  const [selectedTop, setSelectedTop] = useState(0);
+  const [selectedBottoms, setSelectedBottoms] = useState(0);
+  const [selectedShoes, setSelectedShoes] = useState(0);
 
   const handleDress = () => {
     if (dress == false) {
@@ -69,27 +65,23 @@ const Home = () => {
   };
 
   const handleSelectedShoes = (selectedIndex, e) => {
-    // console.log(shoes[selectedIndex]);
     if (selectedShoes != selectedIndex) {
       setSelectedShoes(selectedIndex);
     }
   };
 
   const saveSelectedFavourites = () => {
-    // e.preventDefault();
     const uid = uuidv4();
-    const favourites = {
-      top: tops[selectedTop],
-      bottom: bottoms[selectedBottoms],
-      shoes: shoes[selectedShoes],
+    const favorites = {
+      tops: Object.values(closet[user.uid].tops)[selectedTop],
+      bottoms: Object.values(closet[user.uid].bottoms)[selectedBottoms],
+      shoes: Object.values(closet[user.uid].shoes)[selectedShoes],
     };
-
-    console.log(favourites)
-
-    updateData({ ["/favourites/" + uid]: favourites });
+    updateData({ ["/closet/" + user.uid + "/favorites/" + uid]: favorites });
   };
 
   if (!user) return <h5 className="text-muted">Loading user profile...</h5>;
+  if (!closet) return <h5 className="text-muted">Loading user closet...</h5>;
 
   return (
     <Container>
@@ -113,35 +105,30 @@ const Home = () => {
         Here's what we suggest!
       </Container>
       <Container className="weather-header-container">
-        Today's temperature outside is {weather} degrees with a wind speed of {wind} mph.
+        Today's temperature outside is {weather} degrees with a wind speed of{" "}
+        {wind} mph.
       </Container>
       <Container className="home-clothes-container">
         <Container className="home-clothes-top">
-          {tops && (
-            <MyCarousel
-              data={tops}
-              handleSelect={handleSelectedTop}
-              index={selectedTop}
-            ></MyCarousel>
-          )}
+          <MyCarousel
+            data={closet[user.uid].tops}
+            handleSelect={handleSelectedTop}
+            index={selectedTop}
+          ></MyCarousel>
         </Container>
         <Container className="home-clothes-bottoms">
-          {bottoms && (
-            <MyCarousel
-              data={bottoms}
-              handleSelect={handleSelectedBottoms}
-              index={selectedBottoms}
-            ></MyCarousel>
-          )}
+          <MyCarousel
+            data={closet[user.uid].bottoms}
+            handleSelect={handleSelectedBottoms}
+            index={selectedBottoms}
+          ></MyCarousel>
         </Container>
         <Container className="home-clothes-shoes">
-          {shoes && (
-            <MyCarousel
-              data={shoes}
-              handleSelect={handleSelectedShoes}
-              index={selectedShoes}
-            ></MyCarousel>
-          )}
+          <MyCarousel
+            data={closet[user.uid].shoes}
+            handleSelect={handleSelectedShoes}
+            index={selectedShoes}
+          ></MyCarousel>
         </Container>
       </Container>
       <Container className="home-button-container">
