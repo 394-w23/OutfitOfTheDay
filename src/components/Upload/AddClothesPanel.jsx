470fc0a43f5c38f51ccb7aed55b6c2687200f022
Camilo <camilochafloque@gmail.com>
2023-02-20 20:39:24 -0600

    Finished design for link upload screen

--- src/components/Upload/AddClothesPanel.jsx
+++ src/components/Upload/AddClothesPanel.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import { v4 as uuidv4 } from "uuid";
 import { useStorageUpdate } from "../../utils/firebase";
@@ -10,16 +11,18 @@ import { useProfile } from "../../utils/userProfile";
 const AddClothesPanel = () => {
   //const [user] = useProfile();
   const user = getMockUser();
-  const [type, setType] = useState("jackets");
+  const [type, setType] = useState(null);
+  const [weather, setWeather] = useState(null);
   const [useStorage, result] = useStorageUpdate(
     "/files/" + user.uid + "/" + uuidv4()
   );
   const [updateData] = useDbUpdate("/");
   const [closet] = useDbData("/closet");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const userCloset = closet[user.uid];
+  const handleSubmit = () => {
+    console.log(type);
+    console.log(weather);
+    /*const userCloset = closet[user.uid];
     const userClosetType = userCloset[type];
     const file = e.target[0]?.files[0];
     if (!file) return;
@@ -42,11 +45,83 @@ const AddClothesPanel = () => {
     }
 
     updateData({ ["/closet/" + user.uid]: userCloset });
+    */
   };
 
   if (!user) return <h5 className="text-muted">Loading user profile...</h5>;
 
-  return <Container>Panel</Container>;
+  return (
+    <Container className="mt-2">
+      <Container>
+        <h5 className="link-upload-tag">Category:</h5>
+        <Container className="link-upload-category-container">
+          <Button
+            className={type === "tops" ? "link-upload-button-active" : ""}
+            onClick={() => setType("tops")}
+          >
+            Top
+          </Button>
+          <Button
+            className={type === "jackets" ? "link-upload-button-active" : ""}
+            onClick={() => setType("jackets")}
+          >
+            Jacket
+          </Button>
+          <Button
+            className={type === "bottoms" ? "link-upload-button-active" : ""}
+            onClick={() => setType("bottoms")}
+          >
+            Bottom
+          </Button>
+          <Button
+            className={type === "shoes" ? "link-upload-button-active" : ""}
+            onClick={() => setType("shoes")}
+          >
+            Shoes
+          </Button>
+          <Button
+            className={type === "dresses" ? "link-upload-button-active" : ""}
+            onClick={() => setType("dresses")}
+          >
+            Dress
+          </Button>
+        </Container>
+      </Container>
+      <Container className="mt-2">
+        <h5 className="link-upload-tag">I'll wear this when it's:</h5>
+        <Container className="link-upload-weather-container">
+          <Button
+            className={weather === "cold" ? "link-upload-button-active" : ""}
+            onClick={() => setWeather("cold")}
+          >
+            Cold
+          </Button>
+          <Button
+            className={weather === "warm" ? "link-upload-button-active" : ""}
+            onClick={() => setWeather("warm")}
+          >
+            Warm
+          </Button>
+          <Button
+            className={weather === "rainy" ? "link-upload-button-active" : ""}
+            onClick={() => setWeather("rainy")}
+          >
+            Rainy
+          </Button>
+          <Button
+            className={weather === "sunny" ? "link-upload-button-active" : ""}
+            onClick={() => setWeather("sunny")}
+          >
+            Sunny
+          </Button>
+        </Container>
+      </Container>
+      <Container className="mt-5 link-upload-buttons-container">
+        <Button>Add another Item</Button>
+        <Button onClick={handleSubmit}>I'm done!</Button>
+      </Container>
+    </Container>
+  );
 };
 
 export default AddClothesPanel;
