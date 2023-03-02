c30657e2e7c45e75ea4bd1d25275e794bc85be63
Camilo <camilochafloque@gmail.com>
2023-03-02 09:50:17 -0600

    Fixed small onClick bug in outfit cards

--- src/components/OutfitCard.jsx
+++ src/components/OutfitCard.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import { AiFillHeart } from "react-icons/ai";
 import { TbHanger } from "react-icons/tb";
 import { useDbUpdate } from "../utils/firebase";
 import getMockUser from "../utils/mockUser";
+import OutfitModal from "./OutfitModal";
 
 const OutfitCard = ({ clothes, idx, big }) => {
   const user = getMockUser();
   const [updateData] = useDbUpdate("/");
+  const [show, setShow] = useState(false);
+  const [selectedPiece, setSelectedPiece] = useState(null);
+
+  const handleClose = () => setShow(false);
+  const handleShow = (piece) => {
+    setSelectedPiece(piece);
+    setShow(true);
+  };
 
   const removeFavorite = (idx) => {
     if (!idx) return;
@@ -15,52 +25,62 @@ const OutfitCard = ({ clothes, idx, big }) => {
   };
 
   return (
-    <Card className="mb-3 favorites-card-container">
-      <Container className="favorites-card-clothes">
-        <Container className="favorites-top-shoes-container">
-          <Container className={"favorites-top-container"}>
-            <Card.Img
-              variant="top"
-              src={clothes.tops.url}
-              className={
-                big ? "big-favorites-card-image" : "favorites-card-image"
-              }
-            />
+    <>
+      <Card className="mb-3 favorites-card-container">
+        <Container
+          className="favorites-card-clothes"
+          onClick={() => handleShow(clothes)}
+        >
+          <Container className="favorites-top-shoes-container">
+            <Container className={"favorites-top-container"}>
+              <Card.Img
+                variant="top"
+                src={clothes.tops.url}
+                className={
+                  big ? "big-favorites-card-image" : "favorites-card-image"
+                }
+              />
+            </Container>
+            <Container className={"favorites-shoes-container"}>
+              <Card.Img
+                variant="top"
+                src={clothes.shoes.url}
+                className={
+                  big
+                    ? "big-favorites-card-image-shoes"
+                    : "favorites-card-image-shoes"
+                }
+              />
+            </Container>
           </Container>
-          <Container className={"favorites-shoes-container"}>
+          <Container className={"favorites-bottom-container"}>
             <Card.Img
               variant="top"
-              src={clothes.shoes.url}
+              src={clothes.bottoms.url}
               className={
                 big
-                  ? "big-favorites-card-image-shoes"
-                  : "favorites-card-image-shoes"
+                  ? "big-favorites-card-image-bottom"
+                  : "favorites-card-image-bottom"
               }
             />
           </Container>
         </Container>
-        <Container className={"favorites-bottom-container"}>
-          <Card.Img
-            variant="top"
-            src={clothes.bottoms.url}
-            className={
-              big
-                ? "big-favorites-card-image-bottom"
-                : "favorites-card-image-bottom"
-            }
-          />
+        <Container className="favorites-card-footer">
+          <Container className="favorites-card-frequency">
+            <TbHanger className="me-1" size={23} />
+            {clothes.times}
+          </Container>
+          <Container className="favorites-card-heart">
+            <AiFillHeart onClick={() => removeFavorite(idx)} size={23} />
+          </Container>
         </Container>
-      </Container>
-      <Container className="favorites-card-footer">
-        <Container className="favorites-card-frequency">
-          <TbHanger className="me-1" size={23} />
-          {clothes.times}
-        </Container>
-        <Container className="favorites-card-heart">
-          <AiFillHeart onClick={() => removeFavorite(idx)} size={23} />
-        </Container>
-      </Container>
-    </Card>
+      </Card>
+      <OutfitModal
+        show={show}
+        handleClose={handleClose}
+        clothes={selectedPiece}
+      />
+    </>
   );
 };
 
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,23 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import OutfitCard from "../components/OutfitCard";
 import { useDbData } from "../utils/firebase";
 import getMockUser from "../utils/mockUser";
-import OutfitModal from "../components/OutfitModal";
 
 const Favorites = () => {
   const user = getMockUser();
   const [closet] = useDbData("/closet");
-  const [show, setShow] = useState(false);
-  const [selectedPiece, setSelectedPiece] = useState(null);
-
-  const handleClose = () => setShow(false);
-  const handleShow = (piece) => {
-    setSelectedPiece(piece);
-    setShow(true);
-  };
 
   if (!user) return <h5 className="text-muted">Loading user profile...</h5>;
   if (!closet) return <h5 className="text-muted">Loading user closet...</h5>;
@@ -25,24 +16,15 @@ const Favorites = () => {
   return (
     <Container className="p-10 mt-3">
       <h4 className="closet-title">My Favorites</h4>
-      {
-        <Container>
-          <Row xs={2} md={4}>
-            {Object.entries(closet[user.uid].favorites).map(
-              ([idx, clothes]) => (
-                <Col key={idx} onClick={() => handleShow(clothes)}>
-                  <OutfitCard clothes={clothes} idx={idx} />
-                </Col>
-              )
-            )}
-          </Row>
-        </Container>
-      }
-      <OutfitModal
-        show={show}
-        handleClose={handleClose}
-        clothes={selectedPiece}
-      />
+      <Container>
+        <Row xs={2} md={4}>
+          {Object.entries(closet[user.uid].favorites).map(([idx, clothes]) => (
+            <Col key={idx}>
+              <OutfitCard clothes={clothes} idx={idx} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </Container>
   );
 };
